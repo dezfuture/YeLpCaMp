903bfd74b9a230e58c2655b9e69c4a25a3555213
dezfuture <agrim2000@gmail.com>
2021-06-26 16:35:22 +0530

    here we first introduced the buttons of login etc.... and then wrote the logic to conditionally show these login logout buttons and then we also coded the logic for when the user registers then he doesn

--- routes/users.js
+++ routes/users.js
@@ -8,16 +8,22 @@ router.get("/register", (req, res) => {
   res.render("users/register");
 });
 
+// here this req.login is done as earlier the user has to login again even after he logged out
 router.post(
   "/register",
-  catchAsync(async (req, res) => {
+  catchAsync(async (req, res, next) => {
     try {
       const { email, username, password } = req.body;
       const user = new User({ email, username });
       const registeredUser = await User.register(user, password);
-      console.log(registeredUser);
-      req.flash("success", "Welcome to YeLpCaMp");
-      res.redirect("/campgrounds");
+      //   it requires a callback function so we pass a second function
+      req.login(registeredUser, (err) => {
+        if (err) {
+          return next(err);
+        }
+        req.flash("success", "Welcome to YeLpCaMp");
+        res.redirect("/campgrounds");
+      });
     } catch (e) {
       req.flash("error", e.message);
       res.redirect("register");
@@ -43,10 +49,10 @@ router.post(
   }
 );
 
-// router.get("/logout", (req, res) => {
-//   req.logout();
-//   req.flash("success", "Goodbye!");
-//   res.redirect("/campgrounds");
-// });
+router.get("/logout", (req, res) => {
+  req.logout();
+  req.flash("success", "Goodbye!");
+  res.redirect("/campgrounds");
+});
 
 module.exports = router;
