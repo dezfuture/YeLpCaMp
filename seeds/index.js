52c870964319bdc9df2f7f8a05c674017b4eb4d9
dezfuture <agrim2000@gmail.com>
2021-06-26 22:58:24 +0530

    added the initial authorization and then changed our schema and our index.js

--- seeds/index.js
+++ seeds/index.js
@@ -25,6 +25,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: "60d7091e9e96ff539c979bca",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       // ye 1600x900 hta bhi skte ho, here we have used the unsplash API
