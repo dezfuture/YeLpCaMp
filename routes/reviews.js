aa0172f51c51133ab46cfab48d490db182a929a7
dezfuture <agrim2000@gmail.com>
2021-06-27 01:32:17 +0530

    added the author property to our review model and then added the isLoggedIn middleware to review.js and also conditionally render the review form only if the user is Logged in.

--- routes/reviews.js
+++ routes/reviews.js
@@ -6,14 +6,16 @@ const ExpressError = require("../utils/ExpressError");
 const Campground = require("../models/campground");
 const Review = require("../models/review");
 
-const { validateReview } = require("../middleware");
+const { validateReview, isLoggedIn } = require("../middleware");
 
 router.post(
   "/",
+  isLoggedIn,
   validateReview,
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
