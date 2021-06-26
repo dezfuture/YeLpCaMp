e24ed3f5569045eb3108032f529bf16166eee8fe
dezfuture <agrim2000@gmail.com>
2021-06-27 01:09:15 +0530

    did the same thing for the review middleware

--- routes/reviews.js
+++ routes/reviews.js
@@ -6,15 +6,7 @@ const ExpressError = require("../utils/ExpressError");
 const Campground = require("../models/campground");
 const Review = require("../models/review");
 
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
+const { validateReview } = require("../middleware");
 
 router.post(
   "/",
