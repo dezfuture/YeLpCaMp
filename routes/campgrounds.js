6ccff088d0e36dc34f1ae13f669865affa30cd65
dezfuture <agrim2000@gmail.com>
2021-06-29 14:58:55 +0530

    till here we completed our image uplaod cycle and here we used the url and the filename that multer provides us and using that we make a upload middleware and all.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -9,18 +9,12 @@ const upload = multer({ storage: storage });
 
 const Campground = require("../models/campground");
 
-router
-  .route("/")
-  .get(catchAsync(campgrounds.index))
-  // .post(
-  //   isLoggedIn,
-  //   validateCampground,
-  //   catchAsync(campgrounds.createCampground)
-  // );
-  .post(upload.array("image"), (req, res) => {
-    console.log(req.body, req.files);
-    res.send("IT WORKED");
-  });
+router.route("/").get(catchAsync(campgrounds.index)).post(
+  isLoggedIn,
+  upload.array("image"), //added the upload logic as a middleware
+  validateCampground,
+  catchAsync(campgrounds.createCampground)
+);
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
