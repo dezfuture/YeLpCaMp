8d46d3acc12412dd8b34d1dc25ed5fe3c3b49468
dezfuture <agrim2000@gmail.com>
2021-06-30 13:41:15 +0530

    first fixed the broken seeds and the messed up validation for our campgrounds and then addes a popup to the maps displaying the title and the location of the campground

--- seeds/index.js
+++ seeds/index.js
@@ -33,6 +33,7 @@ const seedDB = async () => {
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
       price,
+      geometry: { type: "Point", coordinates: [80.33111, 26.4725] },
       images: [
         {
           url: "https://res.cloudinary.com/dokje7ogf/image/upload/v1624959705/YeLpCaMp/r5ecbr0dhbxw1p2fnedy.jpg",
